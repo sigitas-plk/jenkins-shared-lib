50c90a3abf4794add7e90c9daf453dec6c4c590a
Sigitas <sigitas@pleikys.com>
2019-04-21 09:14:13 +0100

    feat: reading shell script from file and running in shared lib

---
+++ resources/com/sigi/say
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+case $1 in
+    cat)
+    echo "meow";;
+    dog)
+    echo "wuf";;
+    cow)
+    echo "moo";;
+    *)
+    echo "have no idea";;
+esac
---
+++ vars/sayscript.groovy
@@ -0,0 +1,16 @@
+#!/usr/bin/en groovy
+
+
+
+
+
+def call(String animal = 'cat') {
+    // Lad script from library
+    def bash_script = libraryResource 'com/sigi/say'
+
+    // create a file with bash_script content
+    writeFile file: './say.sh', text: bash_script
+
+    // run the script
+    sh 'bash ./say.sh ${animal}'
+}
